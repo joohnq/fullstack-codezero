ccb4778d2d127947bc3a1b51cb95dfab0f8a97ec
João Henrique <joaohenriquess3287@gmail.com>
2022-11-08 18:22:03 -0300

    feat: Add dotenv config and database mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,5 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(express.json());
+
+app.get("/", (req, res) => res.json({ msg: "Bem vindo a nossa aplicação" }));
+app.listen(port, () => console.log(`Rodando na porta ${port}!`));
