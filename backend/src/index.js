1db02b8c3cd8bc1486933d28b0419683cfd3f2e4
João Henrique <joaohenriquess3287@gmail.com>
2022-11-09 20:10:47 -0300

    feat: Add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 require("./database/config/dbConfig");
 const routes = require("./routes");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
